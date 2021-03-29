d84deda1688c959cd42a1609d11f88386d9af04b
EC2 Default User <sa1225no@gmail.com>
2021-03-29 14:59:42 +0000

    edit 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  protect_from_forgery
+  
   def index
     @books = Book.all
   end
@@ -12,11 +14,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   def create
     book = Book.new(book_params)
     book.save
+    redirect_to book_path(book.id)
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
